d23a211a62ebe9e251b97ca3ed86af46b0fe9350
Vladislav Bakaev <vladislav-bakaev@mail.ru>
2016-09-24 09:38:54 +0400

    Upgrade install script

--- install.py
+++ install.py
@@ -3,7 +3,25 @@ import sys
 import shutil
 
 
-if __name__ == '__main__':
+def install_dependencies():
+    dependencies = ['requests', 'bs4']
+    for dp in dependencies:
+        os.system('pip install {0}'.format(dp))
+        print('{0} installed from pip'.format(dp))
+
+
+def remove_cache(path):
+    cache = '{0}/__pycache__'.format(path)
+    if os.path.exists(cache):
+        shutil.rmtree(cache)
+    for folder in os.listdir(path):
+        next_path = '{0}/{1}'.format(path, folder)
+        if os.path.isdir(next_path):
+            remove_cache(next_path)
+    print('All cache has been removed...')
+
+
+def install_package():
     package = 'eventboost'
     for path in sys.path:
         if os.path.isdir(path) and os.path.abspath(path) != os.getcwd():
@@ -14,3 +32,8 @@ if __name__ == '__main__':
             )
             print('Package has been installed in {0}'.format(path))
             break
+
+if __name__ == '__main__':
+    install_dependencies()
+    remove_cache('.')
+    install_package()
